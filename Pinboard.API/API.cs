e7ea20d560aef294830a39c9b57d905337452e57
Socratees Samipillai <ssamipillai@gmail.com>
2016-04-26 17:10:05 -0500

    Working solution

--- Pinboard.API/API.cs
+++ Pinboard.API/API.cs
@@ -1,9 +1,9 @@
-﻿using Pinboard.Helpers;
-using Pinboard.Types;
-using RestSharp;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using Pinboard.Helpers;
+using Pinboard.Types;
+using RestSharp;
 
 namespace Pinboard
 {
@@ -14,9 +14,6 @@ namespace Pinboard
         private readonly string _token;
         private readonly string _url;
 
-        /// <summary>
-        ///     Instantiates a new Pinboard API client
-        /// </summary>
         public Api(string token)
         {
             _token = token;
@@ -24,10 +21,6 @@ namespace Pinboard
             _client = new RestClient(_url);
         }
 
-        /// <summary>
-        ///     Returns the most recent time a bookmark was added, updated or deleted.
-        ///     Use this before calling posts/all to see if the data has changed since the last fetch.
-        /// </summary>
         public async Task<IRestResponse<UpdateResponse>> GetLastUpdateAsync()
         {
             const string resource = "/posts/update";
@@ -35,9 +28,6 @@ namespace Pinboard
             return await ExecuteAsync<UpdateResponse>(getUpdateRequest);
         }
 
-        /// <summary>
-        ///     Adds a post based on the querystring we pass
-        /// </summary>
         public async Task<IRestResponse<PinboardResponse>> AddPostAsync(Post newPost)
         {
             var addPostRequest = new RestRequest("/posts/add");
@@ -83,9 +73,6 @@ namespace Pinboard
             return await ExecuteAsync<PinboardResponse>(addPostRequest);
         }
 
-        /// <summary>
-        ///     Delete a post via URL
-        /// </summary>
         public async Task<IRestResponse<PinboardResponse>> DeletePost(string url)
         {
             var deletePostRequest = new RestRequest("/posts/delete");
@@ -93,17 +80,11 @@ namespace Pinboard
             return await ExecuteAsync<PinboardResponse>(deletePostRequest);
         }
 
-        /// <summary>
-        ///     Delete a post
-        /// </summary>
         public async Task<IRestResponse<PinboardResponse>> DeletePostAsync(Post deletePost)
         {
             return await DeletePost(deletePost.Href);
         }
 
-        /// <summary>
-        ///     Get posts - filtered by URL, tags or date
-        /// </summary>
         public async Task<IRestResponse<List<Post>>> GetPostsAsync(List<Tag> tags = null, DateTime? createdDate = null,
             string url = null, bool changedMeta = false)
         {
@@ -120,12 +101,9 @@ namespace Pinboard
             return await ExecuteAsync<List<Post>>(getPostsReq);
         }
 
-        /// <summary>
-        ///     Gets most recent posts
-        /// </summary>
         public async Task<IRestResponse<List<Post>>> GetRecentPostsAsync(List<Tag> tags = null, int count = 15)
         {
-            var getPostsReq = new RestRequest("/posts/recent") { RequestFormat = DataFormat.Json };
+            var getPostsReq = new RestRequest("/posts/recent") {RequestFormat = DataFormat.Json};
 
             if (tags != null)
                 getPostsReq.AddParameter("Name", tags.ToString());
@@ -135,9 +113,6 @@ namespace Pinboard
             return await ExecuteAsync<List<Post>>(getPostsReq);
         }
 
-        /// <summary>
-        ///     Gets number of posts by date
-        /// </summary>
         public async Task<IRestResponse<List<DateResponse>>> GetPostDatesAsync(List<Tag> tags = null)
         {
             var getDatesReq = new RestRequest("/posts/dates");
@@ -148,13 +123,10 @@ namespace Pinboard
             return await ExecuteAsync<List<DateResponse>>(getDatesReq);
         }
 
-        /// <summary>
-        ///     Get all posts. This is rate-limited to once every 60 seconds and will return a 429 error if you exceed that
-        /// </summary>
         public async Task<IRestResponse<List<Post>>> GetAllPostsAsync(List<Tag> tags = null, int? start = null,
             int? results = null, DateTime? fromDate = null, DateTime? toDate = null, bool changedMeta = false)
         {
-            var getPostsReq = new RestRequest("/posts/all") { RequestFormat = DataFormat.Json };
+            var getPostsReq = new RestRequest("/posts/all") {RequestFormat = DataFormat.Json};
 
             if (tags != null)
                 getPostsReq.AddParameter(ParameterHelpers.ValueToGetArgument("Name", tags));
@@ -170,18 +142,12 @@ namespace Pinboard
             return await ExecuteAsync<List<Post>>(getPostsReq);
         }
 
-        /// <summary>
-        ///     Returns a full list of the user's tags along with the number of times they were used.
-        /// </summary>
         public async Task<IRestResponse<List<Tag>>> GetTags()
         {
             var getTagsRequest = new RestRequest("/tags/get");
             return await ExecuteAsync<List<Tag>>(getTagsRequest);
         }
 
-        /// <summary>
-        ///     Execute RestRequest, as suggested in the RestSharp documentation
-        /// </summary>
         private async Task<IRestResponse<T>> ExecuteAsync<T>(IRestRequest request) where T : class
         {
             if (_client == null)
@@ -189,8 +155,7 @@ namespace Pinboard
                 throw new NullReferenceException("Client didn't instantiate a RestClient");
             }
 
-            _client.UserAgent = 
-                "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.87 Safari/537.36";
+            _client.UserAgent = "Rest Sharp Client";
             _client.BaseUrl = new Uri(_url);
             _client.Authenticator = new TokenAuthenticator(_token);
 
--- Pinboard.Example/Program.cs
+++ Pinboard.Example/Program.cs
@@ -14,8 +14,8 @@ namespace Pinboard.Example
 
         private static void Main(string[] args)
         {
-            var secret = ConfigurationManager.AppSettings["Secret"];
-            var username = ConfigurationManager.AppSettings["Username"];
+            const string secret = "";
+            const string username = "";
             _client = new Api($"{username}:{secret}");
 
             Console.WriteLine();
@@ -27,145 +27,10 @@ namespace Pinboard.Example
         {
             var tags = await _client.GetTags();
 
-            if (tags?.Data == null)
-            {
-                Console.WriteLine("Error!");
-                return;
-            }
-
             foreach (var  item in tags.Data)
             {
                 Console.WriteLine($"{item.Name}:{item.Count}");
             }
-        }
-
-        private static void GetPinboardMethods()
-        {
-            var api = Assembly.GetAssembly(typeof(Api));
-            var mainType = api.GetType("Pinboard.Api");
-            var methods = mainType.GetMethods(BindingFlags.Instance | BindingFlags.Public | BindingFlags.DeclaredOnly);
-
-            for (int i = 0; i < methods.Length; i++)
-            {
-                Console.WriteLine(i + ":" + methods[i].Name + " " + PrintParameters(methods[i].GetParameters()));
-            }
-
-            try
-            {
-                Console.WriteLine("Choose a method to run (or a number not listed here to exit): ");
-                string selection = Console.ReadLine();
-                int methodNumber = int.Parse(selection.Trim());
-                if (methodNumber > methods.Length + 1 || methodNumber < 0)
-                {
-                    Environment.Exit(1);
-                }
-                List<object> parameterArray = new List<object>();
-                foreach (ParameterInfo methodParam in methods[methodNumber].GetParameters())
-                {
-                    Console.Write("Set the value of {0} (optional: {1}): ", methodParam.Name, methodParam.IsOptional);
-                    var paramValue = Console.ReadLine().Trim();
-
-                    parameterArray.Add(paramValue);
-                }
-
-                try
-                {
-                    var response = methods[methodNumber].Invoke(_client, parameterArray.ToArray());
-                    if (((Task)response).IsFaulted)
-                    {
-                        Console.WriteLine("Fault!");
-                    }
-
-                    response.PrintDump();
-                }
-                catch (TargetInvocationException e)
-                {
-                    Console.WriteLine(e.InnerException.Message);
-                }
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e.Message);
-                Console.WriteLine("Press any key to exit");
-                Console.ReadKey();
-            }
-        }
-
-        private static string PrintParameters(ParameterInfo[] parameterInfo)
-        {
-            StringBuilder builder = new StringBuilder("(");
-            foreach (var info in parameterInfo)
-            {
-                builder.Append(info.Name + ",");
-            }
-            builder.Append(")");
-            return builder.ToString();
-        }
-
-        private static void UpdateSettings(ConfigurationUserLevel configurationLevel = ConfigurationUserLevel.None)
-        {
-            bool needsUsername = false;
-            const string deliciousApi = "https://api.del.icio.us/v1/";
-            Configuration config = ConfigurationManager.OpenExeConfiguration(configurationLevel);
-            Console.WriteLine("Updating app.config at " + config.FilePath);
-            Console.WriteLine("Use which site?");
-            Console.WriteLine("1: Pinboard (default)");
-            Console.WriteLine("2: Delicious");
-            Console.WriteLine("Your selection: ");
-
-            string selection = Console.ReadLine();
-            switch (selection)
-            {
-                case "1": //don't need to update the config
-                    break;
-
-                case "2":
-                    config.AppSettings.Settings.Add("APIBaseURL", deliciousApi);
-                    needsUsername = true; //unlike Pinboard, Delicious uses a username:password pair.
-                    break;
-
-                default:
-                    Console.WriteLine("Invalid selection, using default (Pinboard)");
-                    break;
-            }
-
-            if (needsUsername)
-            {
-                Console.WriteLine("Please enter your username: ");
-                var username = Console.ReadLine();
-                config.AppSettings.Settings.Add("Username", username);
-            }
-
-            string passwordMessage = "Please enter your " + (needsUsername ? "password" : "token") + ":";
-            Console.WriteLine(passwordMessage);
-            config.AppSettings.Settings.Add("token", GetPassword());
-
-            config.Save(ConfigurationSaveMode.Full);
-        }
-
-        private static string GetPassword()
-        {
-            ConsoleKeyInfo key;
-            StringBuilder password = new StringBuilder();
-            do
-            {
-                key = Console.ReadKey(true);
-                if (key.Key != ConsoleKey.Backspace && key.Key != ConsoleKey.Enter)
-                {
-                    password.Append(key.KeyChar);
-                    Console.Write("*");
-                }
-                else
-                {
-                    if (key.Key == ConsoleKey.Backspace && password.Length > 0)
-                    {
-                        password.Remove(password.Length - 1, 1);
-                        Console.Write("\b \b");
-                    }
-                }
-            }
-            while (key.Key != ConsoleKey.Enter);
-            return password.ToString();
         }
     }
 }
